b8ef088eec9a780d5039f5108c2bc1d02293a762
faisal <nmr.njd22@gmail.com>
2022-12-19 18:59:34 +0300

    update deploy

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -4,8 +4,8 @@ eb init aws-api --platform node.js --region us-east-1
 
 eb use aws-api-dev
 
+eb setenv ${AWS_BUCKET} ${AWS_REGION} ${DB_PORT} ${PORT} ${POSTGRES_DB} ${POSTGRES_HOST} ${POSTGRES_PASSWORD} ${POSTGRES_USERNAME} ${AWS_S3_ENDPOINT}
+
 eb deploy aws-api-dev
 
-#echo "*******__ 4.Set Env Variables....👨🏻‍💻🤌🏻  __*******"
-eb setenv AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION DB_PORT=$DB_PORT PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME AWS_S3_ENDPOINT=$AWS_S3_ENDPOINT
 
